9f98d774ebe89195118b111e09cfdb17ab9204b8
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-10-22 19:47:22 +0200

    Update firstAss.js

--- firstAss.js
+++ firstAss.js
@@ -1,5 +1,7 @@
 /// First Assignment ///
 
+
+// Question 1 //
 function extractKey(anArrayOfObj, name){
   
   let result = anArrayOfObj.map(value => {
@@ -14,3 +16,15 @@ function extractKey(anArrayOfObj, name){
 console.log(extractKey([{name: "Elie", isInstructor:true},{name: "Tim", isInstructor:true},{name: "Matt", isInstructor:true}], "name")) // ['Elie', 'Tim', 'Matt']
 
 
+/// Question 2 ///
+function filterLetters(anArrayofLetter, letter){
+  let result = anArrayofLetter.filter((item) => {
+    return item.toLowerCase() === letter.toLowerCase()
+  })
+  
+  return result.length;
+}
+
+console.log(filterLetters(["a","a","b","c","A"], "a")); // 3
+console.log(filterLetters(["a","a","b","c","A"], "z")); // 0
+console.log(filterLetters(["a","a","b","c","A"], "B")); // 1
