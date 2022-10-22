d7b0267998d6c518171000c9b8254badfb0d35e9
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-10-22 20:13:19 +0200

    Update firstAss.js

--- firstAss.js
+++ firstAss.js
@@ -28,3 +28,15 @@ function filterLetters(anArrayofLetter, letter){
 console.log(filterLetters(["a","a","b","c","A"], "a")); // 3
 console.log(filterLetters(["a","a","b","c","A"], "z")); // 0
 console.log(filterLetters(["a","a","b","c","A"], "B")); // 1
+
+
+/// Question 3 ///
+function printFirstAndLast(anArrayOfString){
+  let result = anArrayOfString.map((element) => {
+    console.log(element[0] + element[element.length - 1])
+  })
+  
+  return
+}
+
+console.log(printFirstAndLast(['awesome','example','of','forEach'])) // 'ae' 'ee' 'of' 'fh'
